52879630170aa7c216c18f83443f38522c87565d
rytwork <reimaginedyieldtechnology@gmail.com>
2026-03-06 04:28:12 +0530

    update

--- components/Counter.js
+++ components/Counter.js
@@ -1,14 +1,20 @@
 "use client"
 
 import CountUp from "react-countup"
+import { useInView } from "react-intersection-observer"
 
 export default function Counter({ value, label }) {
 
+    const { ref, inView } = useInView({
+        triggerOnce: true,   // only run once
+        threshold: 0.5       // 50% visible
+    })
+
     return (
-        <div>
+        <div ref={ref}>
 
             <h2 className="text-3xl font-bold text-yellow-400">
-                <CountUp end={value} duration={3} />+
+                {inView && <CountUp end={value} duration={3} />}+
             </h2>
 
             <p className="text-gray-300 text-sm">
